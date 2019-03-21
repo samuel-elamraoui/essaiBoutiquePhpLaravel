7b383b253777a720ab4abdc555283908b7d0f932
Matthias Virat <matthias.virat@Digital-Grenoble.local>
2019-03-21 17:14:06 +0100

    petite correction sur le catalogue

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -10,9 +10,11 @@ Catalogue
     <h1>Catalogue</h1>
 
     <p>Classement par : </p>
+    <div class="article">
     <a href="/produit?sort=price&order=asc"><button type="button">Prix croissant</button> </a>
     <a href="/produit?sort=price&order=desc"><button type="button">Prix décroissant</button> </a>
     <a href="/produit?sort=name&order=asc"><button type="button">Nom</button> </a>
+    </div>
 
     @foreach($produits as $produit)
             <div class="article">
@@ -32,7 +34,6 @@ Catalogue
 
         <br>
     @endforeach
-
 
 
 @endsection
